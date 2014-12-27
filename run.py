160a44e28dc31c71f19783971429ae599b524bcb
Fengda HUANG <h@phodal.com>
2014-12-28 00:04:05 +0800

    generate only commit info

--- run.py
+++ run.py
@@ -3,27 +3,27 @@ from pandas import date_range
 import shlex
 import subprocess as sp
 
-start = datetime(year=2010, month=10, day=24)
+start = datetime(year=2014, month=11, day=24)
 end = datetime.today()
-rng = date_range(start, end, freq='W')
+range = date_range(start, end, freq='W')
 
-cmd = 'git log --oneline --since=%s --until=%s --no-merges'
+cmd = "git log --oneline --since=%s --until=%s --no-merges --pretty=format:'%s'"
 
 
 def commit_message(since_data, util_date):
-    p = sp.Popen(shlex.split(cmd % (since_data, util_date)), stdout=sp.PIPE)
+    p = sp.Popen(shlex.split(cmd % (since_data, util_date, '%s')), stdout=sp.PIPE)
     p.wait()
     return p.stdout.readlines()
 
 
 def get_all_commit_message(date_range):
     commit_messages = []
-    for idx in xrange(len(date_range) - 1):
-        since_date = date_range[idx]
+    for index in xrange(len(date_range) - 1):
+        since_date = date_range[index]
         since_iso = since_date.date().isoformat()
-        until_iso = date_range[idx + 1].date().isoformat()
-        commit_messages.append((since_date, commit_message(since_iso, until_iso)))
+        until_iso = date_range[index + 1].date().isoformat()
+        commit_messages.append((commit_message(since_iso, until_iso)))
     return commit_messages
 
 
-print get_all_commit_message(rng)
+print get_all_commit_message(range)
