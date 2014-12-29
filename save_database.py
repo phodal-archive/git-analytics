86c4335033a9fd33b8c3fa37eb86215ade78e4c2
Fengda HUANG <h@phodal.com>
2014-12-29 23:38:41 +0800

    save commit info to database

--- save_database.py
+++ save_database.py
@@ -1,16 +1,19 @@
 from database import get_pipeline, redis_execute
 from commits import Commit
+import re
 
 commit = Commit()
 
 def save_db():
     pipe = get_pipeline()
     for messages in commit.get_all_commit_message():
-        x = 1
         for msg in messages:
-            print msg
-            redis_execute(pipe, "zincrby", "tw", msg + "", x)
-            x += 1
+            info = re.split(r'[]:]\s*', msg)
+            if info.__len__() is 3 and (info[0].__contains__("CASAXIAN") or info[0].__contains__("CSBAU")):
+                print info[0]
+                redis_execute(pipe, "zincrby", "story", info[0].replace("[", "") + "", 1)
+                redis_execute(pipe, "zincrby", "user", info[1] + "", 1)
+                redis_execute(pipe, "zincrby", "message", info[2] + "", 1)
     pipe.execute()
 
 print save_db()
