13062f931d88f2100a6ea7a22d1984f038b5e8bf
Fengda HUANG <h@phodal.com>
2014-12-30 09:54:33 +0800

    Add simple user api

--- api.py
+++ api.py
@@ -1,17 +1,30 @@
+import json
 from flask import Flask
 from flask.ext import restful
 from run import Info
+from database import redis_execute, get_pipeline
 
 app = Flask(__name__)
 api = restful.Api(app)
+pipe = get_pipeline()
 
 info = Info()
+
 
 class HelloWorld(restful.Resource):
     def get(self):
         return info.bigrams()
 
+
+class User(restful.Resource):
+    def get(self, user_name):
+        redis_execute(pipe, "hget", "user", user_name)
+        result = pipe.execute()
+        return {"count": result[0]}, 201
+
+
 api.add_resource(HelloWorld, '/')
+api.add_resource(User, '/user/<string:user_name>')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- save_database.py
+++ save_database.py
@@ -4,21 +4,26 @@ import re
 
 commit = Commit()
 
+
+def append_message_to_database(info, msg, pipe):
+    if msg.__contains__("CASAXIAN") or msg.__contains__("CSBAU"):
+        redis_execute(pipe, "zincrby", "messages", msg)
+        if info.__len__() is 3:
+            redis_execute(pipe, "zincrby", "story", info[0].replace("[", "") , 1)
+            authors = info[1].split("&")
+            for author in authors:
+                if 1 < author.__len__() < 20:
+                    redis_execute(pipe, "hincrby", "user", author.strip())
+
+            redis_execute(pipe, "hincrby", "message", info[2], 1)
+
+
 def save_db():
     pipe = get_pipeline()
     for messages in commit.get_all_commit_message():
         for msg in messages:
             info = re.split(r'[]:]\s*', msg)
-            if msg.__contains__("CASAXIAN") or msg.__contains__("CSBAU"):
-                redis_execute(pipe, "zincrby", "messages", msg)
-                if info.__len__() is 3:
-                    redis_execute(pipe, "zincrby", "story", info[0].replace("[", "") + "", 1)
-                    authors = info[1].split("&")
-                    for author in authors:
-                        if 1 < author.__len__() < 20:
-                            redis_execute(pipe, "hincrby", "user", author.strip())
-
-                    redis_execute(pipe, "hincrby", "message", info[0] + "", 1)
+            append_message_to_database(info, msg, pipe)
     pipe.execute()
 
 print save_db()
