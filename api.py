6f737dceac3a3839c46cad9f20e2653cbd137da9
Fengda HUANG <h@phodal.com>
2014-12-30 20:39:14 +0800

    add similar user name handle

--- api.py
+++ api.py
@@ -1,8 +1,9 @@
-import json
 from flask import Flask
 from flask.ext import restful
 from run import Info
+import difflib
 from database import redis_execute, get_pipeline
+
 
 app = Flask(__name__)
 api = restful.Api(app)
@@ -12,15 +13,28 @@ info = Info()
 
 
 class HelloWorld(restful.Resource):
-    def get(self):
+    @staticmethod
+    def get():
         return info.bigrams()
 
 
 class User(restful.Resource):
-    def get(self, user_name):
-        redis_execute(pipe, "hget", "user", user_name)
+    @staticmethod
+    def get(user_name):
+        count = 0
+        redis_execute(pipe, "hkeys", "user")
+        user_list = pipe.execute()[0]
+        for user in user_list:
+            # similar user name handle
+            seq = difflib.SequenceMatcher(None, user.lower(), user_name.lower())
+            if seq.ratio() > 0.8:
+                redis_execute(pipe, "hget", "user", user)
         result = pipe.execute()
-        return {"count": result[0]}, 201
+        for res in result:
+            if res != "True":
+                print res
+                count += int(res)
+        return {"name": user_name, "count": count}, 201
 
 
 api.add_resource(HelloWorld, '/')
