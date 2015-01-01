ec2fdce9753fe88ea018737198e34721144d169a
Fengda HUANG <h@phodal.com>
2015-01-01 12:16:31 +0800

    Add POST api

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 from flask.ext import restful
 from flask.ext.cache import Cache
+from flask_restful import reqparse, Resource
 
 from run import Info
 from pr import PR
@@ -14,15 +15,14 @@ info = Info()
 pr = PR()
 all_info = pr.get_info_from_csv(pr, [])
 
-
-class User(restful.Resource):
+class User(Resource):
     @staticmethod
     def get(user_name):
         information = pr.get_user_commit_info(pr, user_name)
         return information, 201
 
 
-class UserInfo(restful.Resource):
+class UserInfo(Resource):
     @staticmethod
     def get(user_name):
         result = []
@@ -31,15 +31,31 @@ class UserInfo(restful.Resource):
         return result, 201
 
 
-class All(restful.Resource):
+class All(Resource):
     @staticmethod
     def get():
         return all_info, 201, {'Access-Control-Allow-Origin': '*'}
+
+parser = reqparse.RequestParser()
+parser.add_argument('user', type=str)
+parser.add_argument('story_number', type=str)
+parser.add_argument('story_type', type=str)
+parser.add_argument('story_title', type=str)
+parser.add_argument('story_day', type=int)
+parser.add_argument('story_description', type=str)
+
+class Story(Resource):
+    @staticmethod
+    def post():
+        args = parser.parse_args()
+        print args
+        return args, 201
 
 
 api.add_resource(User, '/user/<string:user_name>')
 api.add_resource(UserInfo, '/userInfo/<string:user_name>')
 api.add_resource(All, '/all/account')
+api.add_resource(Story, '/story')
 
 if __name__ == '__main__':
     app.run(debug=True)
